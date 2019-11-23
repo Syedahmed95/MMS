0fb606768f15194f299eac4762c5fa86f87b5116
syedahmed95 <syedahmed978@msn.com>
2019-11-24 04:36:22 +0500

    Updates

---
+++ precache-manifest.a63dcb07233ab9d143e80e54c6218b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3021d9300ffb68eae292d5f93977fd1",
+    "url": "/Syedahmed95/MMS/index.html"
+  },
+  {
+    "revision": "7ca7858ebaeed8959cd2",
+    "url": "/Syedahmed95/MMS/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "a3244c4c5581d7b08bf6",
+    "url": "/Syedahmed95/MMS/static/css/main.b13cdec7.chunk.css"
+  },
+  {
+    "revision": "7ca7858ebaeed8959cd2",
+    "url": "/Syedahmed95/MMS/static/js/2.4c3b4b47.chunk.js"
+  },
+  {
+    "revision": "a3244c4c5581d7b08bf6",
+    "url": "/Syedahmed95/MMS/static/js/main.df09cd8a.chunk.js"
+  },
+  {
+    "revision": "9fa39843dd5b5010f20c",
+    "url": "/Syedahmed95/MMS/static/js/runtime-main.2b16da5b.js"
+  }
+]);
